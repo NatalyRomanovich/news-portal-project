90a3a638fe1e2b4eee481c4176be31a7d5c98256
NatalyRomanovich <102173329+NatalyRomanovich@users.noreply.github.com>
2022-07-25 18:49:01 +0300

    Update DoRegistration.java

--- src/main/java/by/htp/jd2/controller/impl/DoRegistration.java
+++ src/main/java/by/htp/jd2/controller/impl/DoRegistration.java
@@ -50,7 +50,7 @@ public class DoRegistration implements Command {
 			}		     
 						
 		}catch (ServiceException e) {
-			//request.getRequestDispatcher().forward(request, response);
+			e.printStackTrace();
 	    }
 	    	
     }
