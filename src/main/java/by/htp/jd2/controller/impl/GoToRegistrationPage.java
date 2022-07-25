f1ecea4babbcaf305b99bad3835a42a9ebf33a65
NatalyRomanovich <102173329+NatalyRomanovich@users.noreply.github.com>
2022-07-25 18:51:14 +0300

    Update GoToRegistrationPage.java

--- src/main/java/by/htp/jd2/controller/impl/GoToRegistrationPage.java
+++ src/main/java/by/htp/jd2/controller/impl/GoToRegistrationPage.java
@@ -19,8 +19,8 @@ public class GoToRegistrationPage implements Command {
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		HttpSession getSession = request.getSession(true);
 		
-		    getSession.setAttribute(AttributsKeys.REG_USER, ConnectionStatus.UNREGISTRED);
-			request.getRequestDispatcher(JspPageName.BASE_PAGE_LAYOUT).forward(request, response);
+		getSession.setAttribute(AttributsKeys.REG_USER, ConnectionStatus.UNREGISTRED);
+		request.getRequestDispatcher(JspPageName.BASE_PAGE_LAYOUT).forward(request, response);
 		
 	}
 
