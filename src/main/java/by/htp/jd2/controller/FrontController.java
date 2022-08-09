0fe7a1084096cc28be9596f1435e6653b07fc4a7
NatalyRomanovich <102173329+NatalyRomanovich@users.noreply.github.com>
2022-08-09 20:09:50 +0300

    Update FrontController.java

--- src/main/java/by/htp/jd2/controller/FrontController.java
+++ src/main/java/by/htp/jd2/controller/FrontController.java
@@ -11,9 +11,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public class FrontController extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-	
-	private final CommandProvider provider = new CommandProvider();
+    private static final long serialVersionUID = 1L;	
+    private final CommandProvider provider = new CommandProvider();
     public ConnectionPool connectionPool;     
     
     public FrontController() {
@@ -22,28 +21,28 @@ public class FrontController extends HttpServlet {
 
     @Override
     public void init () throws ServletException {
-		super.init();
-		try {							
-		     connectionPool = ConnectionPool.getInstance();
+	super.init();
+	try {							
+		connectionPool = ConnectionPool.getInstance();
 						
-		} catch (ConnectionPoolException e) {
-		     // logger.log(Level.ERROR, "Connection not established ");
-		}   
-	} 
+	} catch (ConnectionPoolException e) {
+		// logger.log(Level.ERROR, "Connection not established ");
+	}   
+    } 
     
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String commandName = request.getParameter("command");
-		Command command = provider.getCommand(commandName);
-		command.execute(request, response);
-	}
+	String commandName = request.getParameter("command");
+	ommand command = provider.getCommand(commandName);
+	command.execute(request, response);
+    }
 	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	doGet(request, response);
 	}
 		
 	@Override
     public void destroy() {
-		connectionPool.clearConnectionQueue();
+	connectionPool.clearConnectionQueue();
         super.destroy();
     }
 }
