0c0ba02a8a8ca076ee516a0c49af7155a4360c2b
NatalyRomanovich <102173329+NatalyRomanovich@users.noreply.github.com>
2022-08-22 17:40:31 +0300

    Update FrontController.java

--- src/main/java/by/htp/jd2/controller/FrontController.java
+++ src/main/java/by/htp/jd2/controller/FrontController.java
@@ -20,12 +20,6 @@ public class FrontController extends HttpServlet {
 		super();
 	}
 
-	/*
-	 * @Override public void init() throws ServletException { try {
-	 * ConnectionPool.getInstance(); } catch (ConnectionPoolException e) { //
-	 * logger.log(Level.ERROR, "Connection not established "); } }
-	 */
-
 	protected void doLogic(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		request.getSession(true).setAttribute(AttributsKey.LOCAL, request.getParameter(AttributsKey.LOCAL));
@@ -43,14 +37,5 @@ public class FrontController extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		doLogic(request, response);
-	}
-
-	@Override
-	public void destroy() {
-		try {
-			ConnectionPool.getInstance().clearConnectionQueue();
-		} catch (ConnectionPoolException e) {
-			e.printStackTrace();
-		}
 	}
 }
