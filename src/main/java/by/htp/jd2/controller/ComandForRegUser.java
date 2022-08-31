3e037b15a517e16258825a763b865e8e0dc4cf08
NatalyRomanovich <102173329+NatalyRomanovich@users.noreply.github.com>
2022-08-31 19:00:45 +0300

    Delete src/main directory

--- src/main/java/by/htp/jd2/controller/ComandForRegUser.java
+++
@@ -1,7 +0,0 @@
-package by.htp.jd2.controller;
-
-public enum ComandForRegUser {	
-	
-	GET_USERS_INVALID_PARAMETERS_MAP,
-	GET_USERS_ERROR_MESSAGES_MAP	
-}
--- src/main/java/by/htp/jd2/util/validation/ValidatorBuilder.java
+++
@@ -1,9 +0,0 @@
-package by.htp.jd2.util.validation;
-
-import by.htp.jd2.service.ServiceException;
-import by.htp.jd2.service.ServiceNewsException;
-
-public interface ValidatorBuilder <T> {
-	T build() throws ServiceException, ServiceNewsException;
-
-}
